8c066463eb6cbcd352538447477f7807fee28654
Sinan İci <sinan.ici@bupaacibadem.com.tr>
2025-10-26 23:53:57 +0300

    Add project files.

---
+++ eAppointmentServer.Domain/Entities/Appointment.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eAppointmentServer.Domain.Entities
+{
+    public sealed class Appointment
+    {
+        public Appointment() 
+        {
+            Id = Guid.NewGuid();
+        }
+        public Guid Id { get; set; }
+        public Doctor? DoctorId { get; set; }
+        public Patient? PatientId { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public bool IsCompleted { get; set; }
+    }
+}
